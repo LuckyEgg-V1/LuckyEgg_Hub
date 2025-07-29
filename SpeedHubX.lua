ba76cb66a869b6e9705ee56927bdcf7252fb1d49
LuckyEgg <paxhoneyspark@gmail.com>
2025-07-29 21:32:31 +0800

    Update SpeedHubX.lua

--- SpeedHubX.lua
+++ SpeedHubX.lua
@@ -1,4 +1,3 @@
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/DdiKXZlMwA6dyZCw/raw", true))()
 
