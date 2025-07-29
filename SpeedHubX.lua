1c749eb149c1965da66a44471300dedccb01640d
LuckyEgg <paxhoneyspark@gmail.com>
2025-07-29 21:20:19 +0800

    Update SpeedHubX.lua

--- SpeedHubX.lua
+++ SpeedHubX.lua
@@ -2,7 +2,7 @@ local function hehe()
     loadstring(game:HttpGet("https://cdn.sourceb.in/bins/trPuO4ARHD/0", true))()
 end
 local function loadui()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
 end
 
 task.spawn(hehe)
