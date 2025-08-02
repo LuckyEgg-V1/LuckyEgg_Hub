d12e1b3bb1333fd4e1e534088631044a1acb465a
LuckyEgg <paxhoneyspark@gmail.com>
2025-08-02 14:26:18 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,2 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuckyEgg-V1/LuckyEgg_Hub/refs/heads/main/extras.lua", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LuckyEgg-V1/LuckyEgg_Hub/refs/heads/main/roller.lua", true))()
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/DdiKXZlMwA6dyZCw/raw", true))()
